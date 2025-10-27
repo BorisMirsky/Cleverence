b46e67ff5842d8e0be1a7a45446bd8cd7fca41ad
Boris Mirsky <boris.y.mirsky@gmail.com>
2025-10-27 12:25:29 +0300

    first commit

---
+++ Cleverence1/Cleverence1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+
+public class FindDuplicates
+{
+    public static Boolean FindDuplicatesHashSet<T>(T[] array)
+    {
+        HashSet<T> elements = new HashSet<T>();
+        Boolean result = false;
+        foreach (T element in array)
+        {
+            if (!elements.Add(element))
+            {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 2, 5, 6, 1, 7, 8, 3 };     // true
+        int[] numbers1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };          // false
+        Boolean result = FindDuplicatesHashSet(numbers1);
+        Debug.WriteLine(result.ToString());
+    }
+}
+
+
+
+
+
